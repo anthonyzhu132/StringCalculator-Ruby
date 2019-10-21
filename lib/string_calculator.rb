dfa6bc91b2aa2f11e7a0601d7ff0e0873056132c
Anthony <anthonyzhu132@gmail.com>
2019-10-21 20:31:41 +0000

    implimented feature with ruby to add 2 numbers together and to run rspec to test functionality

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,2 +1,10 @@
 class StringCalculator
+  def self.add(input)
+    if input.empty?
+      0
+    else
+      numbers = input.split(",").map { |num| num.to_i } #Splitting the numbers and then mapping them to num
+      numbers.inject(0) { |sum, number| sum + number } #Using sum and num, and then addding them together 
+    end
+  end
 end
